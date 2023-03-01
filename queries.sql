fb7cc737470fa54afb27a54047517efd28beec54
Iptysam <mukadamiptisam@gmail.com>
2023-03-01 13:37:25 +0300

    Update quests on delete and update

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * from animals WHERE neutered;
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * from animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SAVEPOINT dateofbirth;
+UPDATE animals SET weight_kg=-1*weight_kg;
+ROLLBACK TO dateofbirth;
+UPDATE animals SET weight_kg=-1*weight_kg WHERE weight_kg<0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, count(neutered) as result FROM animals GROUP BY neutered ORDER BY result DESC LIMIT 1;
+SELECT species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
