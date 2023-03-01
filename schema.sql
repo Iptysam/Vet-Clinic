75d437f116ac8469ad75d291976ee8fec54d0f2b
Iptysam <mukadamiptisam@gmail.com>
2023-03-01 13:37:37 +0300

    Add a species column

--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS public.animals
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg numeric
+    weight_kg numeric,
+    species text COLLATE pg_catalog."default"
 )
 
 TABLESPACE pg_default;
