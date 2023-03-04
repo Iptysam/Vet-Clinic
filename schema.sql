9c71fcd949c182fb927c154b87a4ecabadebb456
Iptysam <mukadamiptisam@gmail.com>
2023-03-04 13:08:27 +0300

    Create table vets,visits and specialities

--- schema.sql
+++ schema.sql
@@ -58,3 +58,33 @@ ALTER TABLE animals ADD COLUMN owner_id INT,
   ADD CONSTRAINT fk_owners
   FOREIGN KEY (owner_id)
   REFERENCES owners (id);
+
+  CREATE TABLE IF NOT EXISTS public.vets
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default",
+    age integer,
+    date_of_graduation date,
+    CONSTRAINT vets_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.vets
+    OWNER to postgres;
+
+CREATE TABLE specializations (
+    vet_id INT NOT NULL,
+    species_id INT NOT NULL,
+    PRIMARY KEY (vet_id, species_id),
+    CONSTRAINT FK_vets FOREIGN KEY (vet_id) REFERENCES vets (id),
+    CONSTRAINT FK_species FOREIGN KEY (species_id) REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+    animal_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    visit_date DATE NOT NULL,
+    CONSTRAINT fk_animal_id FOREIGN KEY(animal_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets_id FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
